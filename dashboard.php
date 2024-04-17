f6e7746cc572791969e2fc0800f443f760193271
cygullem <crmchs.gullem.clydecyril@gmail.com>
2024-04-17 20:16:51 +0800

    navbar layout design

--- dashboard.php
+++ dashboard.php
@@ -1,34 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-	<link rel="stylesheet" href="./css/dashboard-style.css">
+    <link rel="stylesheet" href="./css/dashboard-style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="header">
-        <h2>DisLink</h2>
-        <button class="logout-btn">Logout</button>
+        <div class="header_Left">
+            <h2>DisLink</h2>
+        </div>
+
+        <div class="header_Center"></div>
+
+        <div class="header_Right">
+            <div>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+                <i class="fa-brands fa-facebook-messenger"></i>
+            </div>
+            <div>
+                <i class="fa-solid fa-bell"></i>
+            </div>
+            <div class="profile_container" onclick="openProfileMenu()">
+                <img src="Assets/YENZ.png" alt="Profile">
+                <div class="caret">
+                    <i class="fa-solid fa-angle-down"></i>
+                </div>
+            </div>
+        </div>
+        <section class="userMenu_Popup" id="menuPopup">
+            <div class="uMP">
+                <div class="upmcU _settings">
+                    <div class="upmcU_right">
+                        <i class="fa-solid fa-gear"></i>
+                    </div>
+                    <div class="upmcU_left">Settings & privacy</div>
+                </div>
+                <div class="upmcU _helpSupport">
+                    <div class="upmcU_right">
+                        <i class='bx bxs-help-circle'></i>
+                    </div>
+                    <div class="upmcU_left">Help & support</div>
+                </div>
+                <div class="upmcU _giveFeedback">
+                    <div class="upmcU_right">
+                        <i class='bx bxs-comment-error'></i>
+                    </div>
+                    <div class="upmcU_left">Give Feedback</div>
+                </div>
+                <a class="logout-btn" id="logout">
+                    <div class="upmcU_right">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                    </div>
+                    <div class="upmcU_left">Log Out</div>
+                </a>
+            </div>
+        </section>
     </div>
     <div class="main-container">
-    <div class="workspace-column">
-        <h1>Workspace Content</h1>
-    </div>
-    <div class="posted-column">
-        <h1>Posted Content</h1>
-    </div>
-    <div class="friends-column">
-        <div class="friends-list">
-            <p>Ezekiel PElayo  </p>
+        <div class="workspace-column">
+            <h1>Workspace Content</h1>
+        </div>
+        <div class="posted-column">
+            <h1>Posted Content</h1>
+        </div>
+        <div class="friends-column">
+            <div class="friends-list">
+                <p>Ezekiel PElayo </p>
+            </div>
         </div>
     </div>
-    </div>
 
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./js/main.js"></script>
     <script src="./js/script.js"></script>
 </body>
+
 </html>
