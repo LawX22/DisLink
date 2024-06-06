221e44de4813ebf543251c20da9411a8ae8c2342
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-06-06 22:52:21 +0800

    Like feature added but need fixing

--- js/like.js
+++ js/like.js
@@ -1,1 +1,24 @@
+function toggleLike(postId, userId) {
+    console.log("Toggle like function called for post ID: ", postId);
+    
+    // Check if the post is liked or not
+    var isLiked = $("#like-icon-" + postId).hasClass("liked");
+
+    // Send AJAX request
+    $.ajax({
+        type: "POST",
+        url: isLiked ? "unlike.php" : "like.php",
+        data: { postId: postId, userId: userId },
+        success: function(response) {
+            console.log("AJAX call successful. Response: ", response);
+            // Update like count
+            $("#like-count-" + postId).text(response);
+            // Toggle like icon class
+            $("#like-icon-" + postId).toggleClass("liked");
+        },
+        error: function(xhr, status, error) {
+            console.error("AJAX call failed: ", xhr.responseText);
+        }
+    });
+}
 
